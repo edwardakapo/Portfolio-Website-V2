888d7c6962464dff80d23615d4537c5ca3d7355d
Edward Akapo <edwardakapo@outlook.com>
2024-09-24 03:30:33 -0400

    Interactive tooltip working

--- shining-shell/src/components/ui/GithubCalendar.jsx
+++ shining-shell/src/components/ui/GithubCalendar.jsx
@@ -1,11 +1,20 @@
 import GitHubCalendar from 'react-github-calendar';
-
-
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
+import React from 'react';
 
 export default function GithubCalendar(){
     return (
         <>
-            <GitHubCalendar username="edwardakapo"/>
+            <GitHubCalendar username="edwardakapo"
+                renderBlock={(block, activity) =>
+                    React.cloneElement(block, {
+                      'data-tooltip-id': 'react-tooltip',
+                      'data-tooltip-html': `${activity.count} activities on ${activity.date}`
+                    })
+                  }
+            />
+             <ReactTooltip id="react-tooltip" />
         </>
     )
 }
