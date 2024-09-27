362243cca6f9429c15df9fff2fbb29f309d7b6a8
Edward Akapo <edwardakapo@outlook.com>
2024-09-27 03:40:25 -0400

    implemented switching between recent and pinned projects with dropdown, works with css hidden.

--- shining-shell/src/components/ui/GithubRepos.tsx
+++ shining-shell/src/components/ui/GithubRepos.tsx
@@ -2,6 +2,15 @@ import { fetchGitHubPinnedRepos, fetchGitHubRepos } from "@/lib/githubApi";
 import { useState, useEffect } from "react";
 import type { Repository } from "@/lib/githubApi";
 import GithuRepoCard from "./GithubRepoCard";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
 
 const name = "edwardakapo"
 
@@ -24,6 +33,8 @@ export default function GithubRepos() {
 const [pinnedRepos, setPinnedRepos] = useState<Repository[]>([])
 const [recentRepos, setRecentRepos] = useState<Repository[]>([])
 const [isLoading ,setIsLoading] = useState(true)
+const [selectedGrid, setSelectedGrid ]= useState("recent")
+
 useEffect (  () => {
     const getGithubData = async () => {
         try {
@@ -67,28 +78,45 @@ useEffect (  () => {
         return <div> Loading ...</div>
     }
     return (
-        <>
-            <h1> Pinned Repos in here </h1> 
-            <ul className="grid grid-cols-2 gap-y-2 gap-x-2 justify-between">
-                {pinnedRepos.map((repo) => (
-                    <li key={repo.name} className="w-fit">
-                        <GithuRepoCard {...repo}/>
-                    </li>
-                ))
-                }
-            </ul>
-
-            <h1> Recent Repos in here </h1> 
-            <ul className="grid grid-cols-2 gap-y-2 gap-x-2 justify-between">
-                {recentRepos
-                .sort((a,b) => (new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()))
-                .map((repo) => (
-                    <li key={repo.name} className="w-fit">
-                        <GithuRepoCard {...repo}/>
-                    </li>
-                ))
-                }
-            </ul>
-        </>
+        <div className="flex flex-col gap-y-3">
+            <div className="place-self-end">
+                <Select value={selectedGrid} onValueChange={(value) => setSelectedGrid(value)}>
+                    <SelectTrigger className="w-fit rounded-2xl py-1 gap-x-2 border-gray-300 h-fit text-xs font-bold tracking-wider hover:ring-2 hover:ring-gray-300">
+                    <SelectValue/>
+                    </SelectTrigger>
+                    <SelectContent className="max-h-[400px]">
+                        <SelectItem value="recent">Recent Projects</SelectItem>
+                        <SelectItem value="pinned">Pinned Projects</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+            
+            <div className={`${selectedGrid === "pinned" ? "block" : "hidden"}`}>
+                <h1 className="text-sm p-1 mb-1">Projects I want to showcase </h1> 
+                <ul className="grid grid-cols-2 gap-y-2 gap-x-2 justify-between" >
+                    {pinnedRepos.map((repo) => (
+                        <li key={repo.name} className="w-fit">
+                            <GithuRepoCard {...repo}/>
+                        </li>
+                    ))
+                    }
+                </ul>
+            </div>
+            
+            <div className={`${selectedGrid === "recent" ? "block" : "hidden"}`}>
+                <h1 className="text-sm p-1 mb-1"> Latest projects I've been working on</h1> 
+                <ul className="grid grid-cols-2 gap-y-2 gap-x-2 justify-between">
+                    {recentRepos
+                    .sort((a,b) => (new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime()))
+                    .map((repo) => (
+                        <li key={repo.name} className="w-fit">
+                            <GithuRepoCard {...repo}/>
+                        </li>
+                    ))
+                    }
+                </ul>
+            </div>
+        </div>
+        
     )
 }
