833d1f7bde51a2c9bf9754c84d4e76501c0640a2
Edward Akapo <edwardakapo@outlook.com>
2024-10-07 14:41:11 -0400

    updated dialog component to fix mistake made by editing node_module instead of extending type definition to alter types

--- shining-shell/src/components/ui/dialog.tsx
+++ shining-shell/src/components/ui/dialog.tsx
@@ -27,9 +27,12 @@ const DialogOverlay = React.forwardRef<
 ))
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
+interface DialogContentProps extends React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> {
+  useCloseButton?: boolean;
+}
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+  DialogContentProps
 >(({ className, children, useCloseButton=false, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
